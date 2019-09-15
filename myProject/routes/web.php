008f0eab1d936eb8e2747c7973852d58b157c07e
hironori <hironori11an@gmail.com>
2019-09-15 19:04:23 +0900

    　myProject追加

---
+++ myProject/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//トップ画面
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/hello', function () {
+    return view('hello');
+});
+
+Route::get('/hello', function () {
+    return view('hello');
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
