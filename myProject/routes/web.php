0486f3874386753818624392868db07b47aa476f
hironori <hironori11an@gmail.com>
2019-11-22 07:05:38 +0900

    管理ホームurl変更

--- myProject/routes/web.php
+++ myProject/routes/web.php
@@ -29,7 +29,7 @@ Route::get('/hello', function () {
 /* 管理画面 */
 
 Route::get('/loginKanri', 'loginKanriController@init');
-Route::post('/homeKanri', [
+Route::post('/kanri', [
     'uses' => 'loginKanriController@postSignin',
     'as' => 'homeKanri.signin'
     ]);
@@ -37,7 +37,7 @@ Auth::routes();
 //管理ホーム画面
 // Route::group(['middleware' => ['auth', 'can:admin-higher']], function () {
     Route::group(['middleware' => ['can:admin']], function () {
-        Route::get('/homeKanri', function () {
+        Route::get('/kanri', function () {
             return view('kanri.homeKanri');
         })->name('homeKanri');
     });
